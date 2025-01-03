ce56a19fe337b55652df1761ba4762406b60b464
Tomáš Štveráček <tomas.stveracek@gmail.com>
2025-01-03 14:58:46 +0100

    Exchange favicon, logo, setup variables

---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+  return (
+    <img
+      src="/assets/logo.svg"
+      alt="Logo"
+      width={40}
+      height={44}
+      className="logo"
+    />
+  );
+};
+
+export default Logo;
