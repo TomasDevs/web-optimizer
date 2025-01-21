246a75635a96dabc5331f5a1996cac0a716d82f2
Tomáš Štveráček <tomas.stveracek@gmail.com>
2025-01-21 21:28:14 +0100

    Test config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const isMinified = mode === "minified";
+  console.log("API BASE URL:", process.env.VITE_API_URL);
 
   return {
     plugins: [react()],
