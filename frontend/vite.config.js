60605b8dd0099ae50cf085a8a3d329c5fc49d6c6
Tomáš Štveráček <tomas.stveracek@gmail.com>
2025-01-31 21:11:30 +0100

    Edit vite, node version 20

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig(({ mode }) => {
   const isMinified = mode === "minified";
