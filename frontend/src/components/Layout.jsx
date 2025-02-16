5191f6e9ad30008b2c1d7760ce9208ffdb8ffe41
Tomáš Štveráček <tomas.stveracek@gmail.com>
2025-02-16 23:58:12 +0100

    Edit minified version

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -7,11 +7,14 @@ import Additional from "./Additional/Additional";
 
 const Layout = () => {
   const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
 
   // Add minification only to testing pages
   const isTestingPage =
     location.pathname === "/testovani/minifikace-kodu" ||
     location.pathname === "/testovani/komplexni-analyza";
+
+  const isMinified = searchParams.get("minified") !== "false";
 
   return (
     <>
@@ -19,19 +22,11 @@ const Layout = () => {
         <Helmet>
           <link
             rel="stylesheet"
-            href={
-              new URLSearchParams(location.search).get("minified") !== "false"
-                ? "/minified-index.css"
-                : "/unminified-index.css"
-            }
+            href={isMinified ? "/minified-index.css" : "/unminified-index.css"}
           />
           <script
             type="module"
-            src={
-              new URLSearchParams(location.search).get("minified") !== "false"
-                ? "/minified-index.js"
-                : "/unminified-index.js"
-            }
+            src={isMinified ? "/minified-index.js" : "/unminified-index.js"}
           />
         </Helmet>
       )}
