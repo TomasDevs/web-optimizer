a0ed2b9a6f4c572d4f433c08f8787961a450d7a9
Tomáš Štveráček <tomas.stveracek@gmail.com>
2025-02-16 13:25:00 +0100

    Implement un/minified version

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import Header from "./Header";
 import Footer from "./Footer";
 import Additional from "./Additional/Additional";
 
 const Layout = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const isMinified = searchParams.get("minified") !== "false";
+
   return (
     <>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href={isMinified ? "/minified-index.css" : "/unminified-index.css"}
+        />
+        <script
+          type="module"
+          src={isMinified ? "/minified-index.js" : "/unminified-index.js"}
+        />
+      </Helmet>
+
       <div className="container">
         <Header />
         <main>
