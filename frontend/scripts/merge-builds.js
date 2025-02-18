abef3f09c6abf94818f139081850e5fdbf8f1c28
Tomáš Štveráček <tomas.stveracek@gmail.com>
2025-02-18 20:43:20 +0100

    Add minification version to dist/index.html

--- frontend/scripts/merge-builds.js
+++ frontend/scripts/merge-builds.js
@@ -4,32 +4,34 @@ const { copySync, removeSync, readFileSync, writeFileSync } = fsExtra;
 // Cílový adresář pro výsledný build
 const distDir = "dist";
 
-// Smaž existující dist, pokud existuje
+// Smazání existujícího `dist`, pokud existuje
 removeSync(distDir);
 
-// Zkopíruj oba buildy do dist/
+// Kopírování minifikované verze jako hlavní build
 copySync("dist-minified", distDir);
+
+// Přidání neminifikované verze pouze pro testovací podstránky
 copySync("dist-unminified", distDir);
 
 // Smazání pomocných složek
 removeSync("dist-minified");
 removeSync("dist-unminified");
 
-// Oprava index.html – odstranění pevně vložených CSS a JS souborů
+// Úprava `index.html`
 const indexPath = `${distDir}/index.html`;
 let indexContent = readFileSync(indexPath, "utf8");
 
-// Odebrání linků na unminified soubory
+// Odstranění odkazů na `unminified` soubory
 indexContent = indexContent.replace(
   /<script.*?src="\/unminified-index.js".*?><\/script>/g,
-  ""
+  `<script type="module" src="/minified-index.js"></script>`
 );
 indexContent = indexContent.replace(
   /<link.*?href="\/unminified-index.css".*?>/g,
-  ""
+  `<link rel="stylesheet" href="/minified-index.css">`
 );
 
-// Uložit upravený index.html
+// Zajištění, že `index.html` má vždy minifikované soubory
 writeFileSync(indexPath, indexContent, "utf8");
 
 console.log("Soubory úspěšně sloučeny do dist/ a index.html opraven!");
